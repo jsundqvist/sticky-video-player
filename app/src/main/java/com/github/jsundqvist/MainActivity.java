5801648807d80eb0c456b67a719806409f710372
jsundqvist <johannes@codecatalyst.se>
2016-10-02 22:26:15 +0200

    Update MainActivity.java

--- app/src/main/java/com/github/jsundqvist/MainActivity.java
+++ app/src/main/java/com/github/jsundqvist/MainActivity.java
@@ -3,12 +3,47 @@ package com.github.jsundqvist;
 import android.app.*;
 import android.os.*;
 
-public class MainActivity extends Activity 
-{
+public class MainActivity extends Activity {
+    
+	private WebView myWebView;
+
     @Override
-    protected void onCreate(Bundle savedInstanceState)
-    {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+
+		myWebView = (WebView) findViewById(R.id.webview);
+		myWebView.setWebViewClient(new MyWebViewClient());
+		WebSettings webSettings = myWebView.getSettings();
+		webSettings.setJavaScriptEnabled(true);
+		myWebView.loadUrl("https://www.google.com/video");
     }
+
+	private class MyWebViewClient extends WebViewClient
+	{
+		@Override
+		public boolean shouldOverrideUrlLoading(WebView view, String url) {
+			if (Uri.parse(url).getHost().contains("google."))
+				return false;
+			return true;
+		}
+		@Override
+		public void onPageFinished(WebView view, String url) {
+			super.onPageFinished(view, url);
+			hide("div._RAf");
+			hide("div._cy");
+		}
+		
+		private void hide(String selector) {
+			myWebView.evaluateJavascript("document.querySelector('"+selector+"').style.display='none';", null);
+		}
+	}
+
+	@Override
+	public boolean onKeyDown(int keyCode, KeyEvent event) {
+		WebBackForwardList list = myWebView.copyBackForwardList();
+		if (list.getCurrentIndex() > 1)
+			myWebView.goBack();
+		return true;
+	}
 }
